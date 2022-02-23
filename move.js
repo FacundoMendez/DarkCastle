404b767292803ab17faa112a5313cde0b774c219
FacuMendez <facundo.mendez@live.com.ar>
2022-02-23 01:48:11 -0300

    .

--- move.js
+++ move.js
@@ -10,6 +10,86 @@
     
 })();
 
+/* 
+(() =>{
+
+
+})();
+ */
+
+(() =>{
+
+    var html = document.documentElement;
+    var body = document.body;
+    
+    var scroller = {
+      target: document.querySelector("#scroll-container"),
+      ease: 0.05, // <= scroll speed
+      endY: 0,
+      y: 0,
+      resizeRequest: 1,
+      scrollRequest: 0,
+    };
+    
+    var requestId = null;
+    
+    TweenLite.set(scroller.target, {
+      rotation: 0.01,
+      force3D: true
+    });
+    
+    window.addEventListener("load", onLoad);
+    
+    function onLoad() {    
+      updateScroller();  
+      window.focus();
+      window.addEventListener("resize", onResize);
+      document.addEventListener("scroll", onScroll); 
+    }
+    
+    function updateScroller() {
+      
+      var resized = scroller.resizeRequest > 0;
+        
+      if (resized) {    
+        var height = scroller.target.clientHeight;
+        body.style.height = height + "px";
+        scroller.resizeRequest = 0;
+      }
+          
+      var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
+    
+      scroller.endY = scrollY;
+      scroller.y += (scrollY - scroller.y) * scroller.ease;
+    
+      if (Math.abs(scrollY - scroller.y) < 0.9 || resized) {
+        scroller.y = scrollY;
+        scroller.scrollRequest = 0;
+      }
+      
+      TweenLite.set(scroller.target, { 
+        y: -scroller.y 
+      });
+      
+      requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
+    }
+    
+    function onScroll() {
+      scroller.scrollRequest++;
+      if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+      }
+    }
+    
+    function onResize() {
+      scroller.resizeRequest++;
+      if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller);
+      }
+    }
+    
+})();
+
 
  (() =>{
 
@@ -21,16 +101,16 @@
         opacity:0,
     })
  
-    tl.to(".ball",{        
+    tl.to(".ball",{ 
         opacity:3,
         backgrondColor:"black",
         'webkitFilter': 'blur(2rem)',
         scale:.1,       
     })
 
-
-    tl.to(".container",{        
-        opacity:3,
+    tl.to(".container",{ 
+        duration:3,       
+        opacity:1,
     })
 
 
