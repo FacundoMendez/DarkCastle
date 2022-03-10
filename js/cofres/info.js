593d638932649d12258f3cad43731c5edec0f450
Facundo Mendez <facundo.mendez@live.com.ar>
2022-03-10 00:26:20 -0300

    .

--- js/cofres/info.js
+++ js/cofres/info.js
@@ -1,7 +1,3 @@
-let infoGris = document.querySelector(".saberMas-gris");
-let infoVerde = document.querySelector(".saberMas-verde");
-let infoDiamante = document.querySelector(".saberMas-diamante");
-
 
 /* cofre rojo info */
 let infoRojo = document.querySelector(".saberMas-rojo");
@@ -18,19 +14,53 @@ exitRojo.addEventListener("click",function(){
     containerInfoRojo.style.display="none"
     cofreRojoPantalla.style.filter="blur(0rem)"
 })
-/* 
 
 
-infoGris.addEventListener("click",function(){
-    
-})
+/* cofre verde info */
+let infoVerde = document.querySelector(".saberMas-verde");
+let cofreVerdePantalla = document.querySelector(".cofreVerde");
+let containerInfoVerde = document.querySelector(".info-verde")
+let exitVerde = document.querySelector(".exit-verde")
 
 infoVerde.addEventListener("click",function(){
-    
+    containerInfoVerde.style.display="inline-block"
+    cofreVerdePantalla.style.filter="blur(2rem)"
 })
+
+exitVerde.addEventListener("click",function(){
+    containerInfoVerde.style.display="none"
+    cofreVerdePantalla.style.filter="blur(0rem)"
+})
+
+
+/* cofre gris info */
+let infoGris = document.querySelector(".saberMas-gris");
+let cofreGrisPantalla = document.querySelector(".cofreGris");
+let containerInfoGris = document.querySelector(".info-gris")
+let exitGris = document.querySelector(".exit-gris")
+
+infoGris.addEventListener("click",function(){
+    containerInfoGris.style.display="inline-block"
+    cofreGrisPantalla.style.filter="blur(2rem)"
+})
+
+exitGris.addEventListener("click",function(){
+    containerInfoGris.style.display="none"
+    cofreGrisPantalla.style.filter="blur(0rem)"
+})
+
+/* cofre diamond info */
+let infoDiamante = document.querySelector(".saberMas-diamante");
+let cofreDiamantePantalla = document.querySelector(".cofreDiamante");
+let containerInfoDiamante = document.querySelector(".info-diamante")
+let exitDiamante = document.querySelector(".exit-diamante")
 
 infoDiamante.addEventListener("click",function(){
-    
+    containerInfoDiamante.style.display="inline-block"
+    cofreDiamantePantalla.style.filter="blur(2rem)"
 })
 
- */
+exitDiamante.addEventListener("click",function(){
+    containerInfoDiamante.style.display="none"
+    cofreDiamantePantalla.style.filter="blur(0rem)"
+})
